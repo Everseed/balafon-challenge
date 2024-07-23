88fca31d6d3eae0a6c21fe58fe311dd33fe463a8
Steve Ruben <stevetuenkam@gmail.com>
2024-07-23 23:34:27 +0100

    refactoring auth

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -21,11 +21,11 @@ export default async function Page() {
         <div className="flex justify-center">
             <div className="mt-[calc(15vh)] h-fit w-full sm:max-w-md overflow-hidden border-y border-gray-200 sm:rounded-2xl sm:border sm:shadow-xl">
                 <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-                    <a href="https://streakup.vercel.app">
-                        <Image src="/logo.png" width="80" height="80" className="w-16" alt="StreakUp Logo" />
+                    <a href="#">
+                        <Image src="/static/logo.png" width="80" height="80" className="w-16" alt="StreakUp Logo" />
                     </a>
-                    <h3 className="text-xl font-semibold">Sign in to StreakUp</h3>
-                    <p className="text-sm text-gray-500">Start building your new habits here.</p>
+                    <h3 className="text-xl font-semibold">Sign in to Lingo</h3>
+                    <p className="text-sm text-gray-500">Start new conquest.</p>
                 </div>
                 <div className="flex flex-col space-y-3 bg-gray-50 px-4 py-10 sm:px-16">
                     <SignInButton providerId="google">
